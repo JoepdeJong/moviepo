80c595c24bb172676e9546a8123b3c565a60d729
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-08-01 13:11:56 +0200

    Xml feeds

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,26 @@
+import { Link } from 'react-router-dom';
 import EpisodeList from '../Components/EpisodeList/EpisodeList';
 import Header from '../Components/Header/Header';
 import XmlFeedForm from '../Components/XmlFeedForm/XmlFeedForm';
+import { useAppSelector } from '../redux/hooks';
 
 import './Home.scss';
 const Home = () => {
+    const xmlFeeds = useAppSelector(state => state.xmlFeeds);
     return (
         <div className='Home'>
             <Header/>
-            <XmlFeedForm/>
-            <EpisodeList/>
+            {
+                xmlFeeds.feedUrls.length > 0 ?
+                <EpisodeList/> :
+                <>
+                    <div className="block">
+                        <h2>No feeds available.</h2>
+                        <p>Add a feed to start.</p>
+                        <Link to="/feeds" className='button'>Add a feed</Link>
+                    </div>
+                </>
+            }
         </div>
     );
 };
