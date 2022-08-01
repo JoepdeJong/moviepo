82a4fde2bd57ceb42014e842e5b1420f0621d2df
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-08-01 10:35:38 +0200

    Update player

--- src/Components/Player/Player.tsx
+++ src/Components/Player/Player.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactPlayer from 'react-player';
+import { useNavigate } from 'react-router-dom';
 
 import './Player.scss';
 
@@ -8,9 +9,24 @@ type Props = {
 };
 
 const Player = ({id} : Props) => {
+    const navigate = useNavigate();
+
+    // Validate if video stream exists
+    useEffect(() => {
+        //
+    })
+
+    const handleError = (error: any, data: any) => {
+        if(data.response.code === 404) {
+            alert('There is no video stream for this episode.');
+            navigate('/');
+        }
+        console.log(data);
+    }
+
     return (
         <div className='Player'>
-            <ReactPlayer url={`https://cdn.podimo.com/hls-media/${id}/stream_video_high/stream.m3u8`} controls={true} width="100%" height="100%" />
+            <ReactPlayer url={`https://cdn.podimo.com/hls-media/${id}/stream_video_high/stream.m3u8`} controls={true} width="100%" height="100%" onError={handleError} />
         </div>
     );
 };
